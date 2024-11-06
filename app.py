3d4e3621ba099515fe50793b005bab8f69eeb286
Derek Dahl <dahlsystems@gmail.com>
2024-11-05 23:40:30 -0700

    Change records to pizza reviews

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Define a simple model
-class Item(db.Model):
+class PizzaReview(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    score = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
 
 # Create the database
 with app.app_context():
@@ -18,23 +20,25 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    items = Item.query.all()
+    items = PizzaReview.query.all()
     return render_template('index.html', items=items)
 
 @app.route('/add', methods=['POST'])
 def add_item():
     name = request.form['name']
-    new_item = Item(name=name)
+    score = request.form['score']
+    location = request.form['location']
+    new_item = PizzaReview(name=name, score=score, location=location)
     db.session.add(new_item)
     db.session.commit()
     return 'Item Added!'
 
 @app.route('/delete/<int:id>', methods=['GET'])
 def delete_item(id):
-    item = Item.query.get(id)
+    item = PizzaReview.query.get(id)
     db.session.delete(item)
     db.session.commit()
     return 'Item Deleted!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port='6969', debug=True)
